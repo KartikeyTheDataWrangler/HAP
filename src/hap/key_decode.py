5d2417a8572e9c2e6015e8ed1846ecd6e171b4d0
tm kartikey <ikartik27@gmail.com>
2024-01-18 23:18:41 +0530

    dvc gdrive integration complte

--- src/hap/key_decode.py
+++ src/hap/key_decode.py
@@ -1,21 +1,35 @@
 import os
 import base64
 import json
-import subprocess
 from dotenv import load_dotenv
 
 
 
 load_dotenv(".env")
-BASE_64 = os.getenv('base_64')
-#print(BASE_64)
+base_64 = os.getenv('base_64')
+BASE_64 = base_64
+
 class key_decode:
-    def decode_key(self,base64:str):
-        self.base64 = BASE_64
-        decode_json = base64.b64decode(BASE_64).decode('utf-8')
-        #print(decode_json)
+    def decode_key(self,base64_str:str):
+        decode_json = base64.b64decode(base64_str).decode('utf-8')
+        dec_json = json.loads(decode_json)
+        #print(dec_json)
         with open('dvc remote/api_key.json', 'w') as f:
+            json.dump(dec_json,fp=f)
             
+    def del_key(self):
+        file_path = 'dvc remote/api_key.json'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print('directory deleted')
+            
+            
+            
+            
+#import time
+base_64_decode = key_decode().decode_key(base64_str=BASE_64)
+#time.sleep(20)
+key_decode().del_key()
             
 
 
