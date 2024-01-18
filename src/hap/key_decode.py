92cb66b711fcd337f8048fdaadc832eceacf45cb
tm kartikey <ikartik27@gmail.com>
2024-01-18 15:38:56 +0530

    almost complted mlflow gdrive

--- src/hap/key_decode.py
+++ src/hap/key_decode.py
@@ -2,8 +2,21 @@ import os
 import base64
 import json
 import subprocess
-import dotenv
+from dotenv import load_dotenv
 
 
 
-api_secret = SECRET
+load_dotenv(".env")
+BASE_64 = os.getenv('base_64')
+#print(BASE_64)
+class key_decode:
+    def decode_key(self,base64:str):
+        self.base64 = BASE_64
+        decode_json = base64.b64decode(BASE_64).decode('utf-8')
+        #print(decode_json)
+        with open('dvc remote/api_key.json', 'w') as f:
+            
+            
+
+
+
